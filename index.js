0155f7385ca39762f0de1b6daff7ee714bf55d24
unknown <hannanmalik1999@gmail.com>
2022-07-22 22:16:39 +0200

    fix textContent error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",
-"Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0",
- "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+"R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m",
+"n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6",
+ "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[",
  "}","]",",","|",":",";","<",">",".","?","/"];
 let password1 = ""
 let password2 = ""
@@ -12,11 +13,13 @@ function generatePassword() {
         randomPassword1 = Math.floor(Math.random()* characters.length)
         randomPassword2 = Math.floor(Math.random()* characters.length)
         password1 += characters[randomPassword1]
+        password2 += characters[randomPassword2]
+        btn1.textContent += characters[randomPassword1]
         btn2.textContent += characters[randomPassword2]
     }
 }
-function getPasswrod() {
+function getPassword() {
+    btn1.textContent= ""
+    btn2.textContent= ""
     generatePassword()
-
-    btn1.textContent = password1;
 }
