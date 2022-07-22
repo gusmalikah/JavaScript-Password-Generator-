4c1fe1fd1f0ec765ff3ceeafb438acba3b0b77dd
unknown <hannanmalik1999@gmail.com>
2022-07-23 01:29:23 +0200

    check boxes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+const numbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const syabols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let useNumbers = false
+let useSymbols = false
 let password1 = ""
 let password2 = ""
 let btn1 = document.getElementById("btn1")
@@ -25,3 +33,22 @@ function getPassword() {
 function updateSlider() {
     range.textContent = slider.value
 }
+function copyPassword(button) {
+    if(button === "button1"){
+    const range = document.createRange();
+    range.selectNode(document.getElementById("btn1"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    } else if (button === "button2"){
+    const range = document.createRange();
+    range.selectNode(document.getElementById("btn2"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    }
+} 
+
+
