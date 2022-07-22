82b63414cdef81dd8a9f699e2418950bef2af2de
unknown <hannanmalik1999@gmail.com>
2022-07-22 23:01:51 +0200

    Slider Implemented

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
-"R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m",
-"n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6",
- "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[",
- "}","]",",","|",":",";","<",">",".","?","/"];
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 let password1 = ""
 let password2 = ""
 let btn1 = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
+let range = document.getElementById("range")
+let slider = document.getElementById("my-range")
 
 function generatePassword() {
-    for(let i =0; i < 15; i++){
+    for(let i =0; i < slider.value; i++){
         randomPassword1 = Math.floor(Math.random()* characters.length)
         randomPassword2 = Math.floor(Math.random()* characters.length)
         password1 += characters[randomPassword1]
@@ -22,4 +21,7 @@ function getPassword() {
     btn1.textContent= ""
     btn2.textContent= ""
     generatePassword()
+}
+function updateSlider() {
+    range.textContent = slider.value
 }
